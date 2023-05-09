ea2352d8af837310b5ed4d8518fdebbf44dd8eb7
Adrián Nicolás Yáñez Viceiro <yanvicadr@IESPIRINGALLA.AD>
2023-05-09 18:46:05 +0200

    Añadido movimiento con inputs de teclas

--- Assets/Scripts/HelloWorldPlayer.cs
+++ Assets/Scripts/HelloWorldPlayer.cs
@@ -43,7 +43,17 @@ namespace HelloWorld
 
         void Update()
         {
-            
+            if(Input.GetKey(KeyCode.UpArrow)) {
+                transform.position += speed * Vector3.forward * Time.deltaTime;
+            } else if(Input.GetKey(KeyCode.DownArrow)) {
+                transform.position += speed * Vector3.back * Time.deltaTime;
+            } else if(Input.GetKey(KeyCode.LeftArrow)) {
+                transform.position += speed * Vector3.left * Time.deltaTime;
+            } else if(Input.GetKey(KeyCode.RightArrow)) {
+                transform.position += speed * Vector3.right * Time.deltaTime;
+            } else {
+                transform.position += Vector3.zero;
+            }
         }
     }
 }
