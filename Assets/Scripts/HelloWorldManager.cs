b1746cf3f43777366413c0099b8983af44096f51
Adrián Nicolás Yáñez Viceiro <yanvicadr@IESPIRINGALLA.AD>
2023-05-15 18:45:05 +0200

    Modificaciones de ordenación de HelloWorldManager

--- Assets/Scripts/HelloWorldManager.cs
+++ Assets/Scripts/HelloWorldManager.cs
@@ -3,39 +3,27 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace HelloWorld
-{
-    public class HelloWorldManager : MonoBehaviour
-    {
-        void OnGUI()
-        {
+namespace HelloWorld {
+    public class HelloWorldManager : MonoBehaviour {
+        void OnGUI() {
             GUILayout.BeginArea(new Rect(10, 10, 300, 300));
-            if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
-            {
+            if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer) {
                 StartButtons();
-            }
-            else
-            {
+            } else {
                 StatusLabels();
             }
-
             GUILayout.EndArea();
         }
 
-        static void StartButtons()
-        {
+        static void StartButtons() {
             if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
             if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
             if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
         }
 
-        static void StatusLabels()
-        {
-            var mode = NetworkManager.Singleton.IsHost ?
-                "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
-
-            GUILayout.Label("Transport: " +
-                NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
+        static void StatusLabels() {
+            var mode = NetworkManager.Singleton.IsHost ? "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
+            GUILayout.Label("Transport: " + NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
             GUILayout.Label("Mode: " + mode);
         }
     }
