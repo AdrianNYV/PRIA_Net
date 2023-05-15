de8000bd49deb604293779d49f4e5825e99c13a9
Adrián Nicolás Yáñez Viceiro <yanvicadr@IESPIRINGALLA.AD>
2023-05-15 18:38:59 +0200

    Movimiento del player añadido

--- Assets/Scripts/NetworkTransformPlayerEffMov.cs
+++ Assets/Scripts/NetworkTransformPlayerEffMov.cs
@@ -5,5 +5,14 @@ using System;
 using Unity.Netcode;
 
 public class NetworkTransformPlayerEffMov : NetworkBehaviour {
-    
+    public float speed = 5f;
+
+    void Update() {
+        if(IsServer) {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            
+            transform.position = transform.position + new Vector3(x, 0, z) * speed * Time.deltaTime;
+        }
+    }
 }
