b9ce796702cd071d672e634bda3f8762143ea0ba
Adrián Nicolás Yáñez Viceiro <yanvicadr@IESPIRINGALLA.AD>
2023-05-15 18:47:36 +0200

    Eliminado el botón de Server de StartButtons

--- Assets/Scripts/HelloWorldManager.cs
+++ Assets/Scripts/HelloWorldManager.cs
@@ -18,11 +18,10 @@ namespace HelloWorld {
         static void StartButtons() {
             if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
             if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
-            if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
         }
 
         static void StatusLabels() {
-            var mode = NetworkManager.Singleton.IsHost ? "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
+            var mode = NetworkManager.Singleton.IsHost ? "Host" : "Client";
             GUILayout.Label("Transport: " + NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
             GUILayout.Label("Mode: " + mode);
         }
