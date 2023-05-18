2cdf649cddf1a14428efbec3e29345dc98a8386d
Adrián Nicolás Yáñez Viceiro <yanvicadr@IESPIRINGALLA.AD>
2023-05-18 22:20:05 +0200

    Optimización del Movimimiento en ServerRpc

--- Assets/Scripts/NetworkTransformPlayerEffMov.cs
+++ Assets/Scripts/NetworkTransformPlayerEffMov.cs
@@ -13,38 +13,23 @@ public class NetworkTransformPlayerEffMov : NetworkBehaviour {
     }
 
     [ServerRpc]
-    void MoveRightServerRpc(Vector3 dirRight) {
-        transform.position += dirRight * speed * Time.deltaTime;
-    }
-
-    [ServerRpc]
-    void MoveLeftServerRpc(Vector3 dirLeft) {
-        transform.position += dirLeft * speed * Time.deltaTime;
-    }
-
-    [ServerRpc]
-    void MoveForwardServerRpc(Vector3 dirForward) {
-        transform.position += dirForward * speed * Time.deltaTime;
-    }
-
-    [ServerRpc]
-    void MoveBackServerRpc(Vector3 dirBack) {
-        transform.position += dirBack * speed * Time.deltaTime;
+    void MoveServerRpc(Vector3 dir) {
+        transform.position += dir * speed * Time.deltaTime;
     }
 
     void Update() {
         if(IsOwner) {
             if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
-                MoveRightServerRpc(Vector3.right);
+                MoveServerRpc(Vector3.right);
             }
             if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
-                MoveLeftServerRpc(Vector3.left);
+                MoveServerRpc(Vector3.left);
             }
             if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
-                MoveForwardServerRpc(Vector3.forward);
+                MoveServerRpc(Vector3.forward);
             }
             if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
-                MoveBackServerRpc(Vector3.back);
+                MoveServerRpc(Vector3.back);
             }
             if(Input.GetButtonDown("Jump")) {
                 JumpServerRpc();
