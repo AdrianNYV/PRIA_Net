81cd2d85b45e4ccedbbc72d041de7abd7f03e8fe
AdrianNYV <adriannyv@gmail.com>
2023-05-21 19:16:19 +0200

    Añadido variables en HWManager y Base de la Corutina

--- Assets/Scripts/HelloWorldManager.cs
+++ Assets/Scripts/HelloWorldManager.cs
@@ -5,6 +5,14 @@ using System.Collections.Generic;
 
 namespace HelloWorld {
     public class HelloWorldManager : MonoBehaviour {
+        private List<NetworkClient> listOfClientsIds = new List<NetworkClient>();
+
+        private float buff = 2.5f;
+        private float debuff = 0.5f;
+        private float durationOffBD = 10f;
+
+        private bool gameOver = false;
+
         void OnGUI() {
             GUILayout.BeginArea(new Rect(10, 10, 300, 300));
             if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer) {
@@ -16,14 +24,28 @@ namespace HelloWorld {
         }
 
         static void StartButtons() {
-            if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
+            if (GUILayout.Button("Host")) {
+                NetworkManager.Singleton.StartHost();
+                StartCoroutine("IHopeYouFeelLuckyToday");
+            }
             if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+            if (GUILayout.Button("Server")) {
+                NetworkManager.Singleton.StartServer();
+                StartCoroutine("IHopeYouFeelLuckyToday");
+            }
         }
 
         static void StatusLabels() {
-            var mode = NetworkManager.Singleton.IsHost ? "Host" : "Client";
+            var mode = NetworkManager.Singleton.IsHost ? "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
             GUILayout.Label("Transport: " + NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
             GUILayout.Label("Mode: " + mode);
+        }
+
+        IEnumerator IHopeYouFeelLuckyToday() {
+            while(!gameOver) {
+                Debug.Log("Comienzo de Corutina");
+                yield return new WaitForSeconds(20f);
+            }
         }
     }
 }
